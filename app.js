f8a01b6af15b0f2451ef56f1c7c523a2ddec8f1c
Matias Fessia <matiasfessia@gmail.com>
2018-08-30 10:39:47 -0300

    added cors

--- app.js
+++ app.js
@@ -2,8 +2,10 @@
 
 const express = require('express');
 const expressValidator = require('express-validator');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 
 const api = require('./routes');
 app.use(express.json());
